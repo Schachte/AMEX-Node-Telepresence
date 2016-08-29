86bd341dbd0b7050f409effa4d41aea5f5c22733
CheeseQueso <cheesequesadilla@asu.edu>
2016-08-28 21:31:47 -0700

    Added web sockets for statistics update

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -2,29 +2,6 @@
 //Global app variable to represent the application
 var app = angular.module('myApp', []);
 
-app.controller('mainStatistics', function($scope, $http) {
-
-     getStatData();
-
-     function getStatData() {
-
-       //GET request to the server backend
-       $http.get('/getStats').success(function(res) {
-
-         var stats = {
-            active: res[0].active,
-            inactive: res[0].inactive,
-            appointments: res[0].appointments,
-            location_count: res[0].locations
-         }
-
-         //Get JSON response from server of live DB statistics
-         $scope.stats = stats;
-       });
-       setTimeout(getStatData, 10000);
-   }
-});
-
 //Handle form requests for persisting chat data
 app.controller('formCtrl', function($scope, $http) {
   $scope.sendChat = function() {
