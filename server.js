681cabcbfda63d69594498de15c9fb1b790b6db7
CheeseQueso <cheesequesadilla@asu.edu>
2016-08-28 20:43:32 -0700

    Updated server file to include socket.io

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
+/**********************************
+NPM packages (SEE package.json)
+***********************************/
 var express = require('express');
 var path = require('path');
 var logger = require('morgan');
@@ -6,13 +9,26 @@ var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var waterfall = require('async-waterfall');
 var schemas = require('./schemas');
+var http = require('http'),
+    fs = require('fs')
 
+/**********************************
+External File Dependencies
+***********************************/
 var routes_stats = require('./components/stats');
 var routes_chat = require('./components/chat');
 var routes_location = require('./components/location');
 
+/**********************************
+Server Setup
+***********************************/
 var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io').listen(http);
 
+/**********************************
+App usages
+***********************************/
 app.set('port', process.env.PORT || 3000);
 app.use(logger('dev'));
 app.use(bodyParser.json());
@@ -20,9 +36,9 @@ app.use(bodyParser.urlencoded());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-var statData = schemas.statData;
-var chatData = schemas.chatData;
-
+/**********************************
+REST API Endpoints
+***********************************/
 //Grabs the updated statistics for the robots
 app.get('/getStats', routes_stats.statistics);
 
@@ -32,7 +48,9 @@ app.get('/getRobotLocation', routes_location.locationData);
 //Stores chat data from front-end form to DB
 app.post('/storeMessage', routes_chat.storeChatData);
 
-//Start the server
-app.listen(app.get('port'), function() {
-  console.log('Express server listening on port ' + app.get('port'));
+/**********************************
+Run the Server
+***********************************/
+http.listen(app.get('port'), function(){
+  console.log('listening on *:' + app.get('port'));
 });
